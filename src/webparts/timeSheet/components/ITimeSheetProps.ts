49004f04a9c3cd6eb254d7608c446c08fbf968fa
rafadCCS <rafad.aranhikkal@ccs-technologies.com>
2023-05-25 10:16:12 +0530

    commikt

--- src/webparts/timeSheet/components/ITimeSheetProps.ts
+++
@@ -1,7 +0,0 @@
-export interface ITimeSheetProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
--- src/webparts/timeSheetReport/components/ITimeSheetReportProps.ts
+++
@@ -1,7 +0,0 @@
-export interface ITimeSheetReportProps {
-  description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
-  userDisplayName: string;
-}
